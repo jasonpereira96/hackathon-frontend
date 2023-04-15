e3fe51e3c8abe237474ae9771e3d9ed745c3bef7
Anthony Sorto <asorto3@uic.edu>
2023-04-15 16:47:12 -0500

    dashboard file added
    arting

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -10,32 +10,58 @@ function App() {
 
 function DashBoard() {
   return (
-    // <div class="grid grid-rows-3 grid-flow-col gap-4">
-    //   <div class="bg-blue-800 row-span-3 ...">01</div>
-    //   <div class="bg-blue-800 col-span-2 ...">02</div>
-    //   <div class="bg-blue-800 row-span-2 col-span-2 ...">03</div>
-    // </div>
-    <div className="flex max-w-md h-full">
-      <div className="w-1/4 bg-blue-800 flex flex-wrap justify-center">
-        <div className="bg-white m-1 p-1 w-full h-1/2"></div>
-        <div className="bg-white m-1 p-1 w-full h-64">
-          {" "}
-          <LineChart />
-        </div>
+    <div class="grid grid-cols-4 gap-4">
+      <div class="bg-white rounded-lg shadow-lg p-6">
+        <h3 class="text-lg font-medium mb-4">Box 1</h3>
+        <p class="text-gray-500 text-sm">Data 1</p>
       </div>
-
-      <div className="w-1/4 bg-gray-200 flex flex-wrap justify-center">
-        {/* <div className="flex flex-wrap justify-center"> */}
-        <div className="bg-white m-4 p-4 w-64 h-64"></div>
-        <div className="bg-white m-4 p-4 w-full h-64">
-          {" "}
-          <LineChart />
-        </div>
-        {/* </div> */}
+      <div class="bg-white rounded-lg shadow-lg p-6">
+        <h3 class="text-lg font-medium mb-4">Box 2</h3>
+        <p class="text-gray-500 text-sm">Data 2</p>
       </div>
-      <div className="w-1/4 bg-red-800"></div>
-      <div className="w-1/4 bg-green-800"></div>
+      <div class="bg-white rounded-lg shadow-lg p-6">
+        <h3 class="text-lg font-medium mb-4">Box 3</h3>
+        <p class="text-gray-500 text-sm">Data 3</p>
+      </div>
+      <div class="bg-white rounded-lg shadow-lg p-6">
+        <h3 class="text-lg font-medium mb-4">Box 4</h3>
+        <p class="text-gray-500 text-sm">Data 4</p>
+      </div>
+      <div class="bg-white rounded-lg shadow-lg p-6 col-span-2 md:col-span-1">
+        <h3 class="text-lg font-medium mb-4">Box 5</h3>
+        <p class="text-gray-500 text-sm">Data 5</p>
+      </div>
+      <div class="bg-white rounded-lg shadow-lg p-6 col-span-2 md:col-span-1">
+        <h3 class="text-lg font-medium mb-4">Box 6</h3>
+        <p class="text-gray-500 text-sm">Data 6</p>
+      </div>
     </div>
+    // // <div class="grid grid-rows-3 grid-flow-col gap-4">
+    // //   <div class="bg-blue-800 row-span-3 ...">01</div>
+    // //   <div class="bg-blue-800 col-span-2 ...">02</div>
+    // //   <div class="bg-blue-800 row-span-2 col-span-2 ...">03</div>
+    // // </div>
+    // <div className="flex max-w-md h-full">
+    //   <div className="w-1/4 bg-blue-800 flex flex-wrap justify-center">
+    //     <div className="bg-white m-1 p-1 w-full h-1/2"></div>
+    //     <div className="bg-white m-1 p-1 w-full h-64">
+    //       {" "}
+    //       <LineChart />
+    //     </div>
+    //   </div>
+
+    //   <div className="w-1/4 bg-gray-200 flex flex-wrap justify-center">
+    //     {/* <div className="flex flex-wrap justify-center"> */}
+    //     <div className="bg-white m-4 p-4 w-64 h-64"></div>
+    //     <div className="bg-white m-4 p-4 w-full h-64">
+    //       {" "}
+    //       <LineChart />
+    //     </div>
+    //     {/* </div> */}
+    //   </div>
+    //   <div className="w-1/4 bg-red-800"></div>
+    //   <div className="w-1/4 bg-green-800"></div>
+    // </div>
   );
 }
 
