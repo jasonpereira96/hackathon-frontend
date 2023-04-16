a452b5b557552b8b15f41ed2874ee41e66bf9f81
jasonpereira96 <jasonpereira074@gmail.com>
2023-04-16 02:32:09 -0500

    adding set buttons

--- src/pages/components.js
+++ src/pages/components.js
@@ -3,35 +3,86 @@ import { useEffect } from "react";
 import { getData } from "@/api/api";
 
 const options = [
-  { value: "option1", label: "0 Hours" },
-  { value: "option2", label: "1 Hours" },
-  { value: "option3", label: "2 Hours" },
+  { value: 15, },
+  { value: 30, },
+  { value: 45,},
+  { value: 60, },
+  { value: 75, },
+  { value: 90, },
+  { value: 105, },
+  { value: 120, },
 ];
 
 export function Dropdown() {
-  const [selectedOption, setSelectedOption] = useState(options[0]);
+  const [selectedOption1, setSelectedOption1] = useState(options[0]);
+  const [selectedOption2, setSelectedOption2] = useState(options[0]);
 
-  const handleOptionChange = (event) => {
+  const handleOptionChange1 = (event) => {
     const selectedValue = event.target.value;
-    const selected = options.find((option) => option.value === selectedValue);
-    setSelectedOption(selected);
+    const selected = options.find((option) => option.value === parseInt(selectedValue));
+    setSelectedOption1(selected);
   };
+
+  const handleOptionChange2 = (event) => {
+    const selectedValue = event.target.value;
+    const selected = options.find((option) => option.value === parseInt(selectedValue));
+    setSelectedOption2(selected);
+  };
+
 
   return (
     <div>
-      <h2 class="ftext-lg font-large mb-4 text-black"> LIGHTS </h2>
+      <h2 class="ftext-lg font-large mb-4 text-black"> Lights on before sunset </h2>
       <div className="relative inline-block w-full text-gray-700">
         <select
           className="w-full h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none focus:shadow-outline"
-          value={selectedOption.value}
-          onChange={handleOptionChange}
+          value={selectedOption1.value}
+          onChange={handleOptionChange1}
         >
           {options.map((option) => (
             <option key={option.value} value={option.value}>
-              {option.label}
+              {option.value + " minutes"}
             </option>
           ))}
         </select>
+        <br/>
+        <br/>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Set
+        </button>
+        <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
+          <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+            <path
+              fillRule="evenodd"
+              d="M9.293 13.707a1 1 0 001.414 0l4-4a1 1 0 00-1.414-1.414L10 11.586 6.707 8.293a1 1 0 00-1.414 1.414l4 4z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </div>
+      </div>
+      <br/>
+      <br/>
+
+      <br/>
+
+      <h2 class="ftext-lg font-large mb-4 text-black"> Lights off after sunset </h2>
+      <div className="relative inline-block w-full text-gray-700">
+        <select
+          className="w-full h-10 pl-3 pr-6 text-base placeholder-gray-600 border rounded-lg appearance-none focus:shadow-outline"
+          value={selectedOption2.value}
+          onChange={handleOptionChange2}
+        >
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.value + " minutes"}
+            </option>
+          ))}
+        </select>
+        <br/>
+        <br/>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Set
+        </button>
         <div className="absolute inset-y-0 right-0 flex items-center px-2 pointer-events-none">
           <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
             <path
